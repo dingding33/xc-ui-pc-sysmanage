5f95c570c5aea76540e82d7412faa5fab41c2a4b
ydb <yindebo@aliyun.com>
2019-10-23 00:00:26 +0800

    Initial commit

---
+++ src/module/cms/api/cms.js
@@ -0,0 +1,6 @@
+import http from './../../../base/api/public'
+let sysConfig = require('@/../config/sysConfig')
+let apiUrl = sysConfig.xcApiUrlPre;
+export const page_list = (page,size,params) => {
+  return http.requestQuickGet(apiUrl+'/list/'+page+'/'+size)
+}
